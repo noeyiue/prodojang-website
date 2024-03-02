035a7127e6d432f02b1f8c32627d2dee7652fd2e
noeyiue <parat.kantida@gmail.com>
2024-03-02 18:06:14 +0000

    Login feature (ui)

--- app/components/buttons/LoginButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LoginButton = () => {
-  return (
-      <button className='btn font-BaiJamjuree btn-sm sm:btn-sm md:btn-md lg:btn-lg hover:bg-blue-800 bg-navy text-white w-fit'>เข้าสู่ระบบ</button>
-  )
-}
-
-export default LoginButton
---
+++ app/components/form/formtypes.tsx
@@ -0,0 +1,10 @@
+export type LoginInputs = {
+  username: string;
+  password: string;
+};
+
+export type RegisterInputs = {
+  username: string;
+  password: string;
+  cpassword: string;
+};
